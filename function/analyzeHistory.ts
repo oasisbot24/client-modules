759c48ff7a9923441bbefadff630f80147c16bb5
Developerryou <developerryou@gmail.com>
2023-07-24 00:12:50 +0900

    FIX: 오타 수정

--- function/analyzeHistory.ts
+++ function/analyzeHistory.ts
@@ -1,10 +1,10 @@
-import AnalyzeHistory from "@interface/history/HistoryAnalyze";
-import BacktestnputInterface from "@interface/input/BacktestnputInterface";
-import HistoryTrade from "@interface/history/HistoryTrade";
+import AnalyzeHistory from '@interface/history/HistoryAnalyze';
+import BacktestInputInterface from '@interface/input/BacktestInputInterface';
+import HistoryTrade from '@interface/history/HistoryTrade';
 
 function analyzeHistory(
   history: HistoryTrade[],
-  input?: BacktestnputInterface
+  input?: BacktestInputInterface,
 ) {
   let ret: AnalyzeHistory = {
     winrate: {
@@ -57,8 +57,8 @@ function analyzeHistory(
   let durationHolding = 0;
   let last_position = history.length - 1;
   for (let i = history.length - 1; i >= 0; i--) {
-    if (history[i].position === "buy") {
-      if (history[last_position].position === "sell") {
+    if (history[i].position === 'buy') {
+      if (history[last_position].position === 'sell') {
         countWaiting++;
         const buyDate = new Date(history[i].date);
         const sellDate = new Date(history[last_position].date);
@@ -67,10 +67,10 @@ function analyzeHistory(
         last_position = i;
       }
     }
-    if (history[i].position === "sell") {
+    if (history[i].position === 'sell') {
       countTotalPL++;
       if (history[i].profitloss ?? 0 > 0) countProfit++;
-      if (history[last_position].position === "buy") {
+      if (history[last_position].position === 'buy') {
         countHolding++;
         const sellDate = new Date(history[i].date);
         const buyDate = new Date(history[last_position].date);
@@ -103,8 +103,8 @@ function analyzeHistory(
   if (countWaiting > 0) ret.mean_waiting = durationWaiting / countWaiting;
   if (countHolding > 0) ret.mean_holding = durationHolding / countHolding;
   if (countTotalPL > 0) {
-    ret.winrate["수익"] = countProfit / countTotalPL;
-    ret.winrate["손해"] = 1 - countProfit / countTotalPL;
+    ret.winrate['수익'] = countProfit / countTotalPL;
+    ret.winrate['손해'] = 1 - countProfit / countTotalPL;
   }
   Object.keys(ret.tradecoin).map((key, index) => {
     return (ret.tradecoin[key] /= history.length);
@@ -114,7 +114,7 @@ function analyzeHistory(
   // calculating volatility
   const mean_profitlossrate = ret.profitlossrate / duration;
   for (let i = history.length - 1; i >= 0; i--) {
-    if (history[i].position === "sell") {
+    if (history[i].position === 'sell') {
       const diff = (history[i].profitlossrate ?? 0) - mean_profitlossrate;
       ret.volatility += diff * diff;
     }
@@ -129,10 +129,10 @@ function analyzeHistory(
   ret.mean_trading_in_month = parseFloat(ret.mean_trading_in_month.toFixed(1));
   ret.volatility = parseFloat((ret.volatility * 100).toFixed(2));
   ret.max_profitlossrate = parseFloat(
-    (ret.max_profitlossrate * 100).toFixed(2)
+    (ret.max_profitlossrate * 100).toFixed(2),
   );
   ret.min_profitlossrate = parseFloat(
-    (ret.min_profitlossrate * 100).toFixed(2)
+    (ret.min_profitlossrate * 100).toFixed(2),
   );
   return ret;
 }
