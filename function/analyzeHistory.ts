14ba0529bb2c6df14f5fb0518e9cce7c29380a1e
DeveloperRyou <DeveloperRyou@gmail.com>
2023-10-31 00:34:26 +0900

    FIX: analyze 함수 기능 수정

--- function/analyzeHistory.ts
+++ function/analyzeHistory.ts
@@ -1,10 +1,10 @@
-import AnalyzeHistory from "@interface/history/HistoryAnalyze";
-import BacktestInputInterface from "@interface/input/BacktestInputInterface";
-import HistoryTrade from "@interface/history/HistoryTrade";
+import AnalyzeHistory from '@interface/history/HistoryAnalyze';
+import BacktestInputInterface from '@interface/input/BacktestInputInterface';
+import HistoryTrade from '@interface/history/HistoryTrade';
 
 function analyzeHistory(
   history: HistoryTrade[],
-  input?: BacktestInputInterface
+  input?: BacktestInputInterface,
 ) {
   let ret: AnalyzeHistory = {
     winrate: {
@@ -59,32 +59,88 @@ function analyzeHistory(
   let countHolding = 0;
   let durationWaiting = 0;
   let durationHolding = 0;
-  let last_position = history.length - 1;
-  for (let i = history.length - 1; i >= 0; i--) {
-    if (history[i].position === "buy") {
-      if (history[last_position].position === "sell") {
-        countWaiting++;
-        const buyDate = new Date(history[i].date);
-        const sellDate = new Date(history[last_position].date);
-        durationWaiting +=
-          (buyDate.getTime() - sellDate.getTime()) / (1000 * 60 * 60);
-        last_position = i;
+
+  let long_openDate: Date | null = null;
+  let long_closeDate: Date | null = null;
+  let long_duration: number | null = null;
+  let short_openDate: Date | null = null;
+  let short_closeDate: Date | null = null;
+  let short_duration: number | null = null;
+  for (let i = 0; i < history.length; i++) {
+    if (history[i].posSide === 'long') {
+      if (history[i].position === 'buy') {
+        if (long_openDate !== null) {
+          long_openDate = new Date(history[i].date);
+          if (long_closeDate !== null) {
+            long_duration =
+              (long_openDate.getTime() - long_closeDate.getTime()) /
+              (1000 * 60 * 60);
+            long_closeDate = null;
+
+            // add waiting duration
+            countWaiting++;
+            durationWaiting += long_duration ?? 0;
+          }
+        }
       }
-    }
-    if (history[i].position === "sell") {
-      countTotalPL++;
-      if (history[i].profitloss ?? 0 > 0) countProfit++;
-      if (history[last_position].position === "buy") {
+      if (history[i].position === 'sell') {
+        long_closeDate = new Date(history[i].date);
+        if (long_openDate !== null)
+          long_duration =
+            (long_closeDate.getTime() - long_openDate.getTime()) /
+            (1000 * 60 * 60);
+        long_openDate = null;
+
+        // add holding duration
         countHolding++;
-        const sellDate = new Date(history[i].date);
-        const buyDate = new Date(history[last_position].date);
-        durationHolding +=
-          (sellDate.getTime() - buyDate.getTime()) / (1000 * 60 * 60);
-        last_position = i;
+        durationHolding += long_duration ?? 0;
+
+        // caculating pl
+        countTotalPL++;
+        if (history[i].profitloss !== null && history[i].profitloss > 0)
+          countProfit++;
+        if (history[i].type in ret.tradecoin)
+          ret.tradecoin[history[i].type] += 1;
+        else ret.tradecoin[history[i].type] = 1;
+      }
+    } else {
+      if (history[i].position === 'buy') {
+        if (short_openDate !== null) {
+          short_openDate = new Date(history[i].date);
+          if (short_closeDate !== null) {
+            short_duration =
+              (short_openDate.getTime() - short_closeDate.getTime()) /
+              (1000 * 60 * 60);
+            short_closeDate = null;
+
+            // add waiting duration
+            countWaiting++;
+            durationWaiting += short_duration ?? 0;
+          }
+        }
+      }
+      if (history[i].position === 'sell') {
+        short_closeDate = new Date(history[i].date);
+        if (short_openDate !== null)
+          short_duration =
+            (short_closeDate.getTime() - short_openDate.getTime()) /
+            (1000 * 60 * 60);
+        short_openDate = null;
+
+        // add holding duration
+        countHolding++;
+        durationHolding += short_duration ?? 0;
+
+        // caculating pl
+        countTotalPL++;
+        if (history[i].profitloss !== null && history[i].profitloss > 0)
+          countProfit++;
+        if (history[i].type in ret.tradecoin)
+          ret.tradecoin[history[i].type] += 1;
+        else ret.tradecoin[history[i].type] = 1;
       }
     }
-    if (history[i].type in ret.tradecoin) ret.tradecoin[history[i].type] += 1;
-    else ret.tradecoin[history[i].type] = 1;
+
     ret.profitloss += history[i].profitloss ?? 0;
     if (ret.startaccount > 0) {
       ret.nowaccount = ret.startaccount + ret.profitloss;
@@ -107,8 +163,8 @@ function analyzeHistory(
   if (countWaiting > 0) ret.mean_waiting = durationWaiting / countWaiting;
   if (countHolding > 0) ret.mean_holding = durationHolding / countHolding;
   if (countTotalPL > 0) {
-    ret.winrate["수익"] = countProfit / countTotalPL;
-    ret.winrate["손해"] = 1 - countProfit / countTotalPL;
+    ret.winrate['수익'] = countProfit / countTotalPL;
+    ret.winrate['손해'] = 1 - countProfit / countTotalPL;
   }
   Object.keys(ret.tradecoin).map((key, index) => {
     return (ret.tradecoin[key] /= history.length);
@@ -118,7 +174,7 @@ function analyzeHistory(
   // calculating volatility
   const mean_profitlossrate = ret.profitlossrate / duration;
   for (let i = history.length - 1; i >= 0; i--) {
-    if (history[i].position === "sell") {
+    if (history[i].position === 'sell') {
       const diff = (history[i].profitlossrate ?? 0) - mean_profitlossrate;
       ret.volatility += diff * diff;
     }
@@ -133,10 +189,10 @@ function analyzeHistory(
   ret.mean_trading_in_month = parseFloat(ret.mean_trading_in_month.toFixed(1));
   ret.volatility = parseFloat((ret.volatility * 100).toFixed(2));
   ret.max_profitlossrate = parseFloat(
-    (ret.max_profitlossrate * 100).toFixed(2)
+    (ret.max_profitlossrate * 100).toFixed(2),
   );
   ret.min_profitlossrate = parseFloat(
-    (ret.min_profitlossrate * 100).toFixed(2)
+    (ret.min_profitlossrate * 100).toFixed(2),
   );
   return ret;
 }
